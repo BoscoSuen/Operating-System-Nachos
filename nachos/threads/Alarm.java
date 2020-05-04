158bcd0d01fc75fbe4d5b218c422a7f9c92c91e3
Yukinichi <boscosuen@outlook.com>
2020-05-03 20:12:25 -0700

    complete Alarm class

--- nachos/threads/Alarm.java
+++ nachos/threads/Alarm.java
@@ -1,5 +1,6 @@
 package nachos.threads;
 
+import java.util.*;
 import nachos.machine.*;
 
 /**
@@ -15,10 +16,11 @@ public class Alarm {
 	 * <b>Note</b>: Nachos will not function correctly with more than one alarm.
 	 */
 	public Alarm() {
+		waitingQueue = new LinkedList<>();
+		waitingTimeMap = new HashMap<>();
 		Machine.timer().setInterruptHandler(new Runnable() {
 			public void run() {
 				timerInterrupt();
-
 			}
 		});
 	}
@@ -30,7 +32,13 @@ public class Alarm {
 	 * should be run.
 	 */
 	public void timerInterrupt() {
-		KThread.currentThread().yield();
+		boolean disableInterruptResult = Machine.interrupt().disable();
+		long curTime = Machine.timer().getTime();
+		while (!waitingQueue.isEmpty() && waitingTimeMap.get(waitingQueue.peek()) <= curTime) {
+			KThread unblockedThread = waitingQueue.poll();
+			unblockedThread.ready();
+		}
+		Machine.interrupt().restore(disableInterruptResult);
 	}
 
 	/**
@@ -46,10 +54,18 @@ public class Alarm {
 	 * @see nachos.machine.Timer#getTime()
 	 */
 	public void waitUntil(long x) {
+		// the thread must wait for at least X times
 		// for now, cheat just to get something working (busy waiting is bad)
+		// yield: give up the current CPU, put it on the ready queue, will not block!
+		// TODO: use KThread.sleep() to block, use KThread.ready() to unblock  it
+		// TODO: disable interrupt to deal with mutual exclusion
+		// TODO: implement your own waiting queue
 		long wakeTime = Machine.timer().getTime() + x;
-		while (wakeTime > Machine.timer().getTime())
-			KThread.yield();
+		boolean disableInterruptResult = Machine.interrupt().disable();
+		waitingQueue.offer(KThread.currentThread());
+		waitingTimeMap.put(KThread.currentThread(), wakeTime);
+		KThread.sleep();		// block the current thread
+		Machine.interrupt().restore(disableInterruptResult);		// restore the interrupt
 	}
 
         /**
@@ -61,7 +77,10 @@ public class Alarm {
 	 * <p>
 	 * @param thread the thread whose timer should be cancelled.
 	 */
-        public boolean cancel(KThread thread) {
+	public boolean cancel(KThread thread) {
 		return false;
 	}
+
+	private Queue<KThread> waitingQueue;
+	private Map<KThread, Long> waitingTimeMap;
 }
