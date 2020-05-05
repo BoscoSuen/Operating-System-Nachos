d4def50228b514f57f2132a581c5ec6bd028357d
Yukinichi <boscosuen@outlook.com>
2020-05-04 18:27:40 -0700

    Condition Variable

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -2,6 +2,9 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * An implementation of condition variables that disables interrupt()s for
  * synchronization.
@@ -32,8 +35,15 @@ public class Condition2 {
 	public void sleep() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-		conditionLock.release();
+		// add this thread into waiting queue of this CV
+		waitQueue.offer(KThread.currentThread());
 
+		// disable interrupt to make atomic operation
+		boolean intStatus = Machine.interrupt().disable();
+		conditionLock.release();
+		// sleep this thread
+		KThread.sleep();
+		Machine.interrupt().restore(intStatus);
 		conditionLock.acquire();
 	}
 
@@ -43,6 +53,11 @@ public class Condition2 {
 	 */
 	public void wake() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		boolean intStatus = Machine.interrupt().disable();
+		if ((threadTobeWake = waitQueue.poll()) != null) {
+			threadTobeWake.ready();
+		}
+		Machine.interrupt().restore(intStatus);
 	}
 
 	/**
@@ -51,9 +66,14 @@ public class Condition2 {
 	 */
 	public void wakeAll() {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		boolean intStatus = Machine.interrupt().disable();
+		while ((threadTobeWake = waitQueue.poll()) != null) {
+			threadTobeWake.ready();
+		}
+		Machine.interrupt().restore(intStatus);
 	}
 
-        /**
+	/**
 	 * Atomically release the associated lock and go to sleep on
 	 * this condition variable until either (1) another thread
 	 * wakes it using <tt>wake()</tt>, or (2) the specified
@@ -61,9 +81,75 @@ public class Condition2 {
 	 * associated lock.  The thread will automatically reacquire
 	 * the lock before <tt>sleep()</tt> returns.
 	 */
-        public void sleepFor(long timeout) {
+	public void sleepFor(long timeout) {
 		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		waitQueue.offer(KThread.currentThread());
+
+		boolean intStatus = Machine.interrupt().disable();
+
+		conditionLock.release();
+		ThreadedKernel.alarm.waitUntil(timeout);
+		waitQueue.remove(KThread.currentThread());
+		Machine.interrupt().restore(intStatus);
+		conditionLock.acquire();
 	}
 
-        private Lock conditionLock;
+	private Lock conditionLock;
+	private Queue<KThread> waitQueue = new LinkedList<>();
+	private KThread threadTobeWake;
+
+	/** testing
+	 * Example of the "interlock" pattern where two threads strictly
+	 * alternate their execution with each other using a condition
+	 * variable.  (Also see the slide showing this pattern at the end
+	 * of Lecture 6.)
+	 */
+	private static class InterlockTest {
+		private static Lock lock;
+		private static Condition2 cv;
+
+		private static class Interlocker implements Runnable {
+			public void run () {
+				lock.acquire();
+				for (int i = 0; i < 10; i++) {
+					System.out.println(KThread.currentThread().getName());
+					cv.wake();   // signal
+					cv.sleep();  // wait
+				}
+				lock.release();
+			}
+		}
+
+		public InterlockTest () {
+			lock = new Lock();
+			cv = new Condition2(lock);
+
+			KThread ping = new KThread(new Interlocker());
+			ping.setName("ping");
+			KThread pong = new KThread(new Interlocker());
+			pong.setName("pong");
+
+			ping.fork();
+			pong.fork();
+
+			// We need to wait for ping to finish, and the proper way
+			// to do so is to join on ping.  (Note that, when ping is
+			// done, pong is sleeping on the condition variable; if we
+			// were also to join on pong, we would block forever.)
+			// For this to work, join must be implemented.  If you
+			// have not implemented join yet, then comment out the
+			// call to join and instead uncomment the loop with
+			// yields; the loop has the same effect, but is a kludgy
+			// way to do it.
+			ping.join();
+			// for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
+		}
+	}
+
+	// Invoke Condition2.selfTest() from ThreadedKernel.selfTest()
+
+	public static void selfTest() {
+		new InterlockTest();
+	}
+
 }
